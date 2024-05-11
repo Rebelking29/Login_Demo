883b9e749eb3f1e3764466672c3a10fe88153cf5
virus <harshumd29301@gmail.com>
2024-05-11 13:50:39 +0530

    StyleSheet and JavaScript validation is created not linked yet.

---
+++ Login_Demo/src/main/webapp/js/LoginValidation.js
@@ -0,0 +1,42 @@
+function validateLogin() {
+  let username = document.getElementById("username").value;
+  let pass = document.getElementById("pass").value;
+
+  if (username.trim() == "" ) {
+    document.getElementById("usererror").innerText = "*Please Enter Email Id.";
+    return false;
+  } else if (pass.trim() == "" && username != "") {
+    document.getElementById("usererror").innerText = "";
+    document.getElementById("passerror").innerText = "*Please Enter a Password";
+    return false;
+  } else if (pass.length < 8) {
+    document.getElementById("usererror").innerText = "";
+    document.getElementById("passerror").innerText =
+      "*Enter at least 8 characters";
+    return false;
+  } else {
+    document.getElementById("passerror").innerText = "";
+    let user = document.getElementById("username").value;
+    alert("Hey, " + user + " Nice to See You Again :) ");
+    return true;
+  }
+}
+
+function showpass() {
+  let pass = document.getElementById("pass");
+  if (pass.type == "password") {
+    pass.type = "text";
+    document.getElementById("showpass").innerText = "HIDE PASSWORD";
+  } else if (pass.type == "text") {
+    pass.type = "password";
+    document.getElementById("showpass").innerText = "SHOW PASSWORD";
+  }
+}
+
+function hidepass() {
+  let pass = document.getElementById("pass");
+  if (pass.type == "text") {
+    pass.type = "password";
+    document.getElementById("showpass").innerText = "SHOW PASSWORD";
+  }
+}
