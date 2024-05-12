0fbea1c20bea7028f51756bd59b45cc68a9043e0
virus <harshumd29301@gmail.com>
2024-05-12 16:48:58 +0530

    Database Connectivity is Done

---
+++ Login_Demo/src/main/java/com/util/DBConnection.java
@@ -0,0 +1,28 @@
+package com.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+	
+public static Connection getConnection() {
+		
+		Connection con = null;
+		
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/student","sam","password");
+
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+
+}
