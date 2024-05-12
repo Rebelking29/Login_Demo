7d2d1db83560d942c3b139ee063650b0de87fa89
virus <harshumd29301@gmail.com>
2024-05-12 17:03:51 +0530

    Login method implemented Successfully.

--- Login_Demo/src/main/java/com/dao/loginimp.java
+++ Login_Demo/src/main/java/com/dao/loginimp.java
@@ -1,17 +1,45 @@
 package com.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import com.pojo.Login;
+import com.util.DBConnection;
 
 public class loginimp {
 	
+	Connection con;
+	PreparedStatement ps;
+	ResultSet rs;
+	int row;
 	
-	public boolean loginSuccess(Login login){
+	public Login loginSuccess(String username){
+		
+		Login log = null;
+		
+		try {
+			
+			con = DBConnection.getConnection();
+			
+			ps = con.prepareStatement("Select * from login_form where username=?");
+			
+			ps.setString(1, username);
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+					log = new Login();
+					log.setUsername(rs.getString(1));
+					log.setPassword(rs.getString(2));
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		
-		
-		
-		
-		return false;
+		return log;
 		
 	}
 
